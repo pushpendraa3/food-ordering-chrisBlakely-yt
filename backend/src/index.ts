6c8fe5eb3a94724416b091b8c93c46a1bf011939
Pushpendra <pushpendrasingh.91650@gmail.com>
2024-10-16 14:51:19 +0530

    DB connect

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,20 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
+import mongoose from "mongoose";
+
+mongoose.connect(process.env.MONGO_CONNECTION_URI as string)
+.then(() => { console.log("DB CONNECTED!!") })
+.catch((err) => { console.log("problem connecting DB!", err) });
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
+
+
 app.get("/", (req: Request, res: Response) => { res.json("Hi!! from Backend.") })
 
-app.listen("3000", () => console.log("backend is up an running"))
+app.listen("3000", () => {
+    console.log("Backend is up an running");
+})
